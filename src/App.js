7c90a99b9236fcfb9cbf6736a07b0a2c7f6cf7e1
knopgm <knop.gm@gmail.com>
2022-07-30 17:11:48 +0200

    Fix NumberOfEvents interaction and test it

--- src/App.js
+++ src/App.js
@@ -2,15 +2,57 @@ import React, { Component } from "react";
 import "./App.css";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
+
+import { extractLocations, getEvents } from "./api";
 import NumberOfEvents from "./NumberOfEvents";
 
 class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+    number: "",
+  };
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents =
+        location === "all"
+          ? events
+          : events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents,
+      });
+    });
+  };
+
+  handleInputChange = (inputValue) => {
+    this.setState({
+      number: inputValue,
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <CitySearch />
-        <NumberOfEvents />
-        <EventList />
+        <CitySearch
+          locations={this.state.locations}
+          updateEvents={this.updateEvents}
+        />
+        <NumberOfEvents onChange={this.handleInputChange} />
+        <EventList events={this.state.events} number={this.state.number} />
       </div>
     );
   }
--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -2,25 +2,27 @@ import React, { Component } from "react";
 
 class NumberOfEvents extends Component {
   state = {
-    number: "",
+    inputValue: "",
   };
-
-  handleInputChanged = (event) => {
-    this.setState({
-      number: event.target.value,
-    });
-  };
-
   render() {
+    const { onChange } = this.props;
     return (
       <div className="numberOfEvents">
         Number of Events:
         <input
           type="number"
           className="inputNumber"
-          value={this.state.number}
-          onChange={this.handleInputChanged}
+          value={this.state.inputValue}
+          onChange={(event) =>
+            this.setState({ inputValue: event.target.value })
+          }
         />
+        <button
+          className="numberOfEvents-btn"
+          onClick={() => onChange(this.state.inputValue)}
+        >
+          Confirm
+        </button>
       </div>
     );
   }
